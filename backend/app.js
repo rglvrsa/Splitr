78e4cec11b7236c5eb726221cdd77d9c4095a069
rgjc12 <rohitgupta35757@gmail.com>
2025-12-20 02:50:13 +0530

    Small Changes

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,33 @@ connectDB();
 
 const app = express();
 
-// Middleware
+
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
-  credentials: true,
-}));
+      origin: [        
+        "http://localhost:5173",
+        "http://localhost:8000"
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      credentials: true,
+  }));
+  
+  
+  
+  app.options('*', cors());
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204); 
+    } else {
+      next();
+    }
+  });
+
 app.use(express.json());
 app.use(cookieParser());
 
